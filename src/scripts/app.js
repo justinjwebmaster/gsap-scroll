4087ad6a9b4ef7c8a2bccfd3a926594a52ad95a1
jwebmaster <justin@jwebmaster.be>
2023-03-29 11:03:50 +0200

    Apparition image en grid avec effet de rotation, scale, et opacity

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,19 +3,40 @@ import ScrollTrigger from 'gsap/Scrolltrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const list = gsap.utils.toArray(".img"); //faire un tableau de toute mes .box
+const listLeft = gsap.utils.toArray(".img--left"); //faire un tableau de toute mes .box
+const listRight = gsap.utils.toArray(".img--right"); //faire un tableau de toute mes .box
+// fait une boucle sur ma liste
+listRight.forEach(function(imgRight) { // item = 1 element de la liste
+  gsap.from(imgRight, {
+    opacity: .0,
+    duration: .4,
+    scale: .4,
+    rotate: -2,
+    scrollTrigger: {
+      trigger :imgRight,
+      start: "top 75%",
+      end: "bottom 50%",
+      toggleActions: "play pause resume reset",
+      // markers: true,
+      scrub: .5,
+    }
+  });
+});
 
 // fait une boucle sur ma liste
-list.forEach(function(item) { // item = 1 element de la liste
-  gsap.to(item, {
-    opacity: 1,
+listLeft.forEach(function(imgLeft) { // item = 1 element de la liste
+  gsap.from(imgLeft, {
+    opacity: .0,
     duration: .4,
-    scale: 1,
+    scale: .6,
+    rotate: 2,
     scrollTrigger: {
-      trigger :item,
-      start: "top center",
+      trigger :imgLeft,
+      start: "top 75%",
+      end: "center 50%",
       toggleActions: "play pause resume reset",
-      markers: true,
+      // markers: true,
+      scrub: .5,
     }
   });
 });
