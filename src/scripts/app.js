5b71ea691f786ee5cab25e90ec3b95ff2cbdae3e
jwebmaster <justin@jwebmaster.be>
2023-03-29 10:32:02 +0200

    Apparition image en flex

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,22 @@
-console.info('Hello world');
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/Scrolltrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const list = gsap.utils.toArray(".img"); //faire un tableau de toute mes .box
+
+// fait une boucle sur ma liste
+list.forEach(function(item) { // item = 1 element de la liste
+  gsap.to(item, {
+    opacity: 1,
+    duration: .6,
+    scrollTrigger: {
+      trigger :item,
+      start: "center center",
+      toggleActions: "play pause resume reset",
+      markers: true,
+    }
+  });
+});
+
+console.log(gsap.version);
