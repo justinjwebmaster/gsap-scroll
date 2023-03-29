7c01aa37701aa60720fab53914a64e09e2ce81bf
jwebmaster <justin@jwebmaster.be>
2023-03-29 12:40:25 +0200

    Page animation ok

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,35 +3,49 @@ import ScrollTrigger from 'gsap/Scrolltrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const listLeft = gsap.utils.toArray(".img--left"); //faire un tableau de toute mes .box
-const listRight = gsap.utils.toArray(".img--right"); //faire un tableau de toute mes .box
+gsap.from(".header", {
+  duration: .8,
+  scale: .6,
+  scrollTrigger: {
+    trigger :".header",
+    start: "top top",
+    end: "bottom top",
+    toggleActions: "play pause resume reset",
+    markers: true,
+    scrub: .5,
+  }
+});
+
+
+
+// Animation des images des cards
+const listLeft = gsap.utils.toArray(".card__imgContainer"); //faire un tableau de toute mes .box
+
 // fait une boucle sur ma liste
-listRight.forEach(function(imgRight) { // item = 1 element de la liste
-  gsap.from(imgRight, {
-    opacity: .0,
-    duration: .4,
-    scale: .4,
-    rotate: -2,
+listLeft.forEach(function(imgContainer) { // item = 1 element de la liste
+  
+  const selector = gsap.utils.selector(imgContainer);
+  const img = selector(".card__img");
+  
+  gsap.from(imgContainer, {
+    duration: .8,
+    rotate: -4,
     scrollTrigger: {
-      trigger :imgRight,
+      trigger :imgContainer,
       start: "top 75%",
-      end: "bottom 50%",
+      end: "center 50%",
       toggleActions: "play pause resume reset",
       // markers: true,
       scrub: .5,
     }
   });
-});
 
-// fait une boucle sur ma liste
-listLeft.forEach(function(imgLeft) { // item = 1 element de la liste
-  gsap.from(imgLeft, {
-    opacity: .0,
-    duration: .4,
-    scale: .6,
-    rotate: 2,
+  gsap.from(img, {
+    opacity: .6,
+    duration: .8,
+    scale: 1,
     scrollTrigger: {
-      trigger :imgLeft,
+      trigger : img,
       start: "top 75%",
       end: "center 50%",
       toggleActions: "play pause resume reset",
@@ -40,5 +54,20 @@ listLeft.forEach(function(imgLeft) { // item = 1 element de la liste
     }
   });
 });
+
+
+gsap.to(".textSlide__text", {
+  duration: .8,
+  x: "-250vw",
+  scrollTrigger: {
+    trigger : ".textSlide__text",
+    start: "top bottom",
+    end: "bottom top",
+    toggleActions: "play pause resume reset",
+    // markers: true,
+    scrub: .5,
+  }
+});
+
 
 console.log(gsap.version);
